5d90f2412f08d281f0fd258165d3f07f0d4bce25
bdaniele1 <111710795+bdaniele1@users.noreply.github.com>
2022-10-05 14:48:12 -0300

    Create Estoque.java

---
+++ Estoque.java
@@ -0,0 +1,23 @@
+public class Estoque {
+
+    private Estoque fitnes;
+
+    public Estoque() {
+        fitnes = new Estoque();
+    }
+
+    public static void main(String[] args) {
+        String reposiçao;
+        String categoriasRoupas;
+
+        System.out.println("Dentro do nosso estoque temos:" + categoriasRoupas);
+        System.out.println("Estocamos também:" + reposiçao);
+        System.out.println("O estque total possui:" + (categoriasRoupas + reposiçao));
+
+    }
+
+    public Estoque AdicionarNovaCategoria(String categoriaRoupa) {
+        return (fitnes);
+
+    }
+}
