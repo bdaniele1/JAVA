29c7c5c9b82b09070b3f761428ab0f34e9cc5b29
bdaniele1 <111710795+bdaniele1@users.noreply.github.com>
2022-10-05 14:47:49 -0300

    Create Contato.java

---
+++ Contato.java
@@ -0,0 +1,23 @@
+class Contato {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        String email, site;
+        String telefone;
+
+        email = "todachic@nossoemail.com.br";
+        site = "https://www.lojachic.com.br/portaldoconsumidor";
+        telefone = "71-998779315";
+
+        System.out.println("Nossos clientes sempre serão nossa preferencia!");
+        System.out.println("Contatos da nossa loja:");
+
+        System.out.println("Entre em contato com a nossa loja por meio de nosso email:" + email);
+        System.out.println("Entre em contato com nossa loja por meio do nosso site:" + site);
+        System.out.println("Entre em contato com nossa loja por meio do nosso telefone para contato:" + telefone);
+
+    }
+}
