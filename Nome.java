c96b77b225802bf65f6fe2b210d66801555532a9
bdaniele1 <111710795+bdaniele1@users.noreply.github.com>
2022-10-05 14:48:45 -0300

    Create Nome.java

---
+++ Nome.java
@@ -0,0 +1,15 @@
+class NomedaLoja {
+    public static void main(String[] args) {
+
+        String nome, proprietario, descriçao;
+
+        nome = "Toda Chic.";
+        proprietario = "BiankRoucher.";
+        descriçao = "Loja de roupas voltada para o publico feminino.";
+
+        System.out.println("Loja:" + nome);
+        System.out.println("Proprietario:" + proprietario);
+        System.out.println("Descriçao:" + descriçao);
+
+    }
+}
